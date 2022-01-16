dd05a428d16e626393c77a7530a4a0a44166431c
jordans6 <jordanschneider@telus.net>
2022-01-16 11:15:38 -0800

    nav bar fix

--- mytinerary/src/components/Home.js
+++ mytinerary/src/components/Home.js
@@ -8,7 +8,7 @@ function Body(){
 
   return (
     <Container bg={bg}>
-        <NavBar />
+        
         <Wrapper>
             <Header>
                 Personalized Itineraries
@@ -57,7 +57,7 @@ const Container = styled.div`
 `;
 
 const Wrapper = styled.div`
-    padding: 0rem 0rem 20rem 2rem;
+    padding: 10rem 0rem 20rem 2rem;
     color: #FFF;
     text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
